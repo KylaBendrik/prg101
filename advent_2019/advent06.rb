c7e53aabf7a991e89ef0fcf4191a19fefc141fa1
Beth Ashton <Mbeth.ashton@gmail.com>
2019-12-17 20:33:20 -0500

    done with part 1

--- advent_2019/advent06.rb
+++ advent_2019/advent06.rb
@@ -1,90 +1,18 @@
-# frozen_string_literal: true
+input = File.read('./advent06_input.txt')
 
-class Orbit
-  attr_accessor :star, :planet
-  def initialize(star, planet)
-    @star = star
-    @planet = planet
-
-    # display
-  end
-
-  def display
-    puts "#{@planet} is in orbit around #{@star}"
-  end
+orbits = {}
+input.lines.each do |line|
+  host, sat = line.split(')')
+  orbits[host] ||= []
+  orbits[host].push sat.chomp
 end
 
-class Body
-  attr_accessor :children, :id
-  def initialize(id)
-    @id = id
-    @children = []
-  end
-  def add_child(child)
-    @children.push(child)
-  end
+def num_orbits(orbits, current, depth)
+  sats = orbits[current]
+  return depth if sats.nil? || sats.empty?
+  sats
+    .map {|sat| num_orbits(orbits, sat, depth + 1)}
+    .sum + depth
 end
 
-class Map
-  attr_accessor :orbit_list
-
-  def initialize(input)
-    @orbit_list = []
-    @bodies = []
-    @direct_orbits = 0
-    @indirect_orbits = 0
-    input.each do |line|
-      array = line.split(')')
-      star = array[0]
-      planet = array[1].chomp
-      @orbit_list.push(Orbit.new(star, planet))
-    end
-
-    def check_body(id_num)
-      unless @bodies.all? { |body|
-        body.id != id_num
-      } do
-        new_body = Body.new(orbit[0])
-        return new_body
-      end
-    end
-
-    def find_all_bodies
-      #go through orbit list. If we don't have the body yet, assign, if we do, add child
-      @orbit_list.each do |orbit|
-
-        unless @bodies.all? { |body|
-          body.id != orbit[0]
-        } do
-          new_body = Body.new(orbit[0])
-          @bodies.push(new_body)
-        end
-
-        body1 = check_body(orbit[0])
-        body2 = check_body(orbit[1])
-        
-        unless @bodies.all? { |body|
-          body.id != orbit[1]
-        } do
-          new_body = Body.new(orbit[1])
-          @bodies.push(new_body)
-        end
-      
-      end
-    end
-  end
-
-  def count_direct
-
-  end
-
-  def display
-    @orbit_list.each(&:display)
-  end
-end
-
-input = File.read('./advent06_input.txt').lines
-puts input[0]
-puts input[1]
-
-map = Map.new(input)
+puts num_orbits(orbits, 'COM', 0)
