df169242c3c128ece0252562166b578a8806c6af
Beth Ashton <Mbeth.ashton@gmail.com>
2019-12-21 19:24:09 -0500

    doing exactly the opposite of intended goal
    Wanting to keep single letters and vowel clusters, not consonants

--- frequency.rb
+++ frequency.rb
@@ -4,24 +4,57 @@ input = File.read('./frequency_input.txt').downcase
 
 # input is string of words: "dangsin-ege insa, chinjeol"
 # split into array, getting rid of all punctuation:[dansin, ege, insa, chinjeol]
-array = input.tr(',.-?-!', ' ').split(' ')
+input = input.split /\W+/
+puts input
 
 CONSONANTS = %w[b c d f g h j k l m n p q r s t v w x z].freeze
 VOWELS = %w[a e i o u y].freeze
 
-def count_clusters(array)
-  clusters = Hash.new(0)
-  new_cluster = []
-  char_type = "empty"
+class Counter
+  def count_clusters(array)
+    clusters = Hash.new(0)
+    new_cluster = []
+    char_type = 'empty'
 
-  array.each do |word|
-    puts "#{word}"
-    word.chars.each do |char|
-      clusters[char] +=1
+    array.each do |word|
+      last_char_type = :empty
+      current_char_type = :empty
+      current_cluster = []
+
+      word.chars.each do |char|
+        clusters[char] += 1 # always count single letters
+
+        current_char_type = if VOWELS.include?(char)
+                              :vowel
+                            else
+                              :consonant
+                            end
+
+        # if we're at the beginning of the word, just add letter to cluster
+        # if last letter is the same type as this letter, add letter to cluster
+        # if last letter is different, submit cluster, start new cluster, add letter
+
+        unless last_char_type == :empty || last_char_type == current_char_type
+          print current_char_type
+          if current_cluster.length > 1 && current_char_type == :vowel
+            print " - submitting"
+            clusters[current_cluster.join] += 1
+          end
+          puts " "
+          current_cluster = []
+        end
+
+        current_cluster.push(char)
+
+        last_char_type = current_char_type
+        current_char_type = :empty
+      end
     end
-  end
 
-  clusters
+    clusters
+  end
 end
 
-puts count_clusters(['ace'])
+my_counter = Counter.new
+
+puts my_counter.count_clusters(input)
--- spec/frequency_spec.rb
+++ spec/frequency_spec.rb
@@ -3,8 +3,21 @@
 require 'spec_helper'
 require_relative '../frequency'
 
-describe count_clusters(array) do
-  it "gets {'a' => 1, 'c' => 1, 'e' => 1} for 'ace'" do
-    expect(count_clusters(['ace'])).to eq {"a"=>1, "c"=>1, "e"=>1})
+describe Counter do
+  let(:counter) { described_class.new }
+  it "gets {'a' => 1, 'c' => 1, 'e' => 1} for ['ace']" do
+    input = ['ace']
+
+    expect(counter.count_clusters(input)).to eq({"a"=>1, "c"=>1, "e"=>1})
+  end
+
+  it "gets {'a'=>2, 'c'=>1, 'e'=>1, 't'=>1} for ['ace', 'at']" do
+    input = ['ace', 'at']
+    expect(counter.count_clusters(input)).to eq({"a"=>2, "c"=>1, "e"=>1, "t"=>1})
+  end
+
+  it "gets {'h'=>1, 'e'=>1, 'l'=>2, 'o'=>1} for ['hello']" do
+    input = ['hello']
+    expect(counter.count_clusters(input)).to eq({'h'=>1, 'e'=>1, 'l'=>2, 'o'=>1})
   end
 end
