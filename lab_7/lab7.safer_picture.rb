e5e15c10501bece402ae2122225e90fd23445e12
Beth Ashton <Mbeth.ashton@gmail.com>
2020-01-09 12:17:24 -0500

    pictures system primed and ready

--- lab_7/lab7.safer_picture.rb
+++ lab_7/lab7.safer_picture.rb
@@ -1,25 +1,64 @@
 # frozen_string_literal: true
 
-Dir.chdir 'C:\Users\BethJackson\Desktop\learn\lab_7\inputs\new_pictures'
+CATEGORIES = {
+  'bark' => 0,
+  'leaves' => 0,
+  'moss' => 0
+}
 
-pic_names = Dir['..\pictures\*.jpg']
+def rename(old_name, file_type, batch_name, default_num)
+  puts old_name  
+
+  CATEGORIES.each do |cat, num|
+    if old_name.downcase.include?(cat)
+
+      extra = if old_name.downcase.include?('seamless')
+        '_seamless'
+      else
+        ''
+      end
+     
+      num += 1
+      CATEGORIES[cat] = num
+      puts "adding to category: #{cat} ##{num}"
+      return "#{cat}/#{cat}#{num}#{extra}.#{file_type}"
+    end
+  end
+
+  default_num += 1
+  puts "no category found. Default Number assigned: #{default_num}"
+  return "#{batch_name}#{default_num}.#{file_type}"
+
+end
+
+Dir.chdir 'C:/Users/BethJackson/Desktop/learn/lab_7/inputs/new_pictures'
+
+pic_names_jpgs = Dir['../pictures/*.jpg']
+pic_names_pngs = Dir['../pictures/*.png']
 
 puts 'What would you like to call this batch?'
 
 batch_name = gets.chomp
 
 puts
-print "Downloading #{pic_names.length} files:  "
+print "Downloading #{pic_names_jpgs.length + pic_names_pngs.length} files:  "
 
 pic_number = 1
+default_number = 0
 
-pic_names.each do |_name|
+pic_names_jpgs.each do |name|
   print '.'
-  new_name = if pic_number < 10
-               "#{batch_name}0#{pic_number}.jpg"
-             else
-               "#{batch_name}#{pic_number}.jpg"
-             end
+  new_name = rename(name, 'jpg', batch_name, default_number)
+
+  File.rename name, new_name
+
+  pic_number += 1
+end
+
+pic_names_pngs.each do |name|
+  print '.'
+  new_name = rename(name, 'png', batch_name, default_number)
+
 
   File.rename name, new_name
 
@@ -28,3 +67,5 @@ end
 
 puts
 puts 'Done!'
+
+
