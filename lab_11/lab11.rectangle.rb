269ef00cc4e0982e5b96d76895eb43a83f05a88c
Beth Ashton <Mbeth.ashton@gmail.com>
2020-02-03 19:30:25 -0500

    Actually done with lab 11!

---
+++ lab_11/lab11.rectangle.rb
@@ -0,0 +1,32 @@
+class Rectangle
+  attr_reader :width, :height
+
+  def width=(value)
+    if value < 0
+      raise "Width can't be negative!"
+    end
+    @width = value
+  end
+
+  def height=(value)
+    if value < 0
+      raise "Height can't be negative!"
+    end
+    @height = value
+  end
+
+  def initialize(width:, height:)
+    self.width = width
+    self.height = height
+  end
+
+  def area
+    width * height
+  end
+end
+
+rectangle = Rectangle.new(width: 2, height: 4)
+puts rectangle.area # Output: 8
+
+rectangle1 = Rectangle.new(4, 4)
+puts rectangle1.area
