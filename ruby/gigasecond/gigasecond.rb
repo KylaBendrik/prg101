1801b3a519455f36aad4e73b5290d2bfa40d3d39
Beth Ashton <Mbeth.ashton@gmail.com>
2020-02-05 17:01:16 -0500

    fixing gigasecond

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,6 +1,6 @@
 class Gigasecond
   GIGASECONDS = 1_000_000_000
-  def self.from(start_time_in_seconds)
-    start_time_in_seconds + GIGASECONDS
+  def self.from(seconds)
+    seconds + GIGASECONDS
   end
 end
